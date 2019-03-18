f61354e530a92b1cc0910a644ab0d6b55e827022
RagnarLodbrok2017 <35165213+RagnarLodbrok2017@users.noreply.github.com>
2019-03-18 16:59:17 +0200

    make (users) table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,18 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->default('avatar.jpg');
+
+            //            status = active or not
+            $table->string('status')->default('active')->change();
+
+            //           type = admin or superadmin or user or vip
+            $table->string('type')->default('user')->change();
+
+
+            $table->string('location')->nullable();
+            $table->integer('age')->nullable();
+            $table->string('username')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
